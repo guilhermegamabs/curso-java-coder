f049679a1959253241a4cfd70d3fc7faf5f81a88
Guilherme Gama <guilhermegamabs@gmail.com>
2024-04-15 16:53:00 -0300

    Modificadores de Acesso (Getters/Setters)

---
+++ src/POO/encapsulamento/Pessoa.java
@@ -0,0 +1,33 @@
+package classe.src.POO.encapsulamento;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+
+    Pessoa(String nome, int idade) {
+        setNome(nome);
+        setIdade(idade);
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Olá eu sou o " + getNome() + ",\nTenho " + getIdade() + " anos!";
+    }
+}
