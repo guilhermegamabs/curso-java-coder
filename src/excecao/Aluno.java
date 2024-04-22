29d7f024a8625d5f30607375e4549e7425f62143
Guilherme Gama <guilhermegamabs@gmail.com>
2024-04-22 14:56:52 -0300

    Exceção Personalizada

--- src/excecao/Aluno.java
+++ src/excecao/Aluno.java
@@ -1,17 +1,17 @@
 package classe.src.excecao;
 
 public class Aluno {
-    final String nome;
-    final double nota;
-    final boolean bomCoportamento;
+    public final String nome;
+    public final double nota;
+    public final boolean bomCoportamento;
 
-    Aluno(String nome, double nota, boolean bomCoportamento) {
+    public Aluno(String nome, double nota, boolean bomCoportamento) {
         this.nome = nome;
         this.nota = nota;
         this.bomCoportamento = bomCoportamento;
     }
 
-    Aluno(String nome, double nota) {
+    public Aluno(String nome, double nota) {
         this(nome, nota, true);
     }
 
---
+++ src/excecao/personalizadaA/StringVaziaException.java
@@ -0,0 +1,14 @@
+package classe.src.excecao.personalizadaA;
+
+@SuppressWarnings("serial")
+public class StringVaziaException extends RuntimeException{
+    private String nomeDoAtributo;
+
+    public StringVaziaException(String nomeDoAtributo){
+        this.nomeDoAtributo = nomeDoAtributo;
+    }
+
+    public String getMessage() {
+        return String.format("O atributo '%s' está vázio", nomeDoAtributo);
+    }
+}
